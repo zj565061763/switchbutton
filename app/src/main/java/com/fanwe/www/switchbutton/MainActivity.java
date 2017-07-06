0a3b3471af0b1c213b47b797805a7d4ecc2179ab
zjun <565061763@qq.com>
2017-07-06 10:57:25 +0800

    demo新增在RecyclerView中的展示效果

--- app/src/main/java/com/fanwe/www/switchbutton/MainActivity.java
+++ app/src/main/java/com/fanwe/www/switchbutton/MainActivity.java
@@ -2,17 +2,26 @@ package com.fanwe.www.switchbutton;
 
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
+import android.view.ViewGroup;
 
 import com.fanwe.library.SDLibrary;
 import com.fanwe.library.activity.SDBaseActivity;
+import com.fanwe.library.adapter.SDSimpleRecyclerAdapter;
+import com.fanwe.library.adapter.viewholder.SDRecyclerViewHolder;
+import com.fanwe.library.model.SelectableModel;
 import com.fanwe.library.switchbutton.ISDSwitchButton;
 import com.fanwe.library.switchbutton.SDSwitchButton;
 import com.fanwe.library.utils.LogUtil;
 import com.fanwe.library.utils.SDToast;
+import com.fanwe.library.view.SDRecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends SDBaseActivity
 {
     private SDSwitchButton sb;
+
 
     @Override
     protected void init(Bundle savedInstanceState)
@@ -21,8 +30,9 @@ public class MainActivity extends SDBaseActivity
         setContentView(R.layout.activity_main);
         sb = (SDSwitchButton) findViewById(R.id.sb);
 
-        sb.setDebug(true);
+        testRecyclerView();
 
+        sb.setDebug(true);
         sb.setOnCheckedChangedCallback(new SDSwitchButton.OnCheckedChangedCallback()
         {
             @Override
@@ -46,5 +56,45 @@ public class MainActivity extends SDBaseActivity
         });
 
 //        sb.setChecked(true, false, false);
+    }
+
+    private SDRecyclerView rv_content;
+
+    private void testRecyclerView()
+    {
+        rv_content = (SDRecyclerView) findViewById(R.id.rv_content);
+
+        List<SelectableModel> listModel = new ArrayList<>();
+        for (int i = 0; i < 100; i++)
+        {
+            SelectableModel model = new SelectableModel();
+            listModel.add(model);
+        }
+
+        SDSimpleRecyclerAdapter<SelectableModel> adapter = new SDSimpleRecyclerAdapter<SelectableModel>(listModel, this)
+        {
+            @Override
+            public void onBindData(SDRecyclerViewHolder<SelectableModel> holder, int position, final SelectableModel model)
+            {
+                SDSwitchButton sb = holder.get(R.id.sb);
+                sb.setDebug(true);
+                sb.setChecked(model.isSelected(), false, false);
+                sb.setOnCheckedChangedCallback(new ISDSwitchButton.OnCheckedChangedCallback()
+                {
+                    @Override
+                    public void onCheckedChanged(boolean checked, SDSwitchButton view)
+                    {
+                        model.setSelected(checked);
+                    }
+                });
+            }
+
+            @Override
+            public int getLayoutId(ViewGroup parent, int viewType)
+            {
+                return R.layout.item_listview;
+            }
+        };
+        rv_content.setAdapter(adapter);
     }
 }
