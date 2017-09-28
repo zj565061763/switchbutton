c10e544b501e9a72ce0b7be6b811503c090fdce2
zjun <565061763@qq.com>
2017-09-28 22:36:57 +0800

    重构包名

--- lib/src/main/java/com/fanwe/lib/switchbutton/ISDSwitchButton.java
+++ lib/src/main/java/com/fanwe/lib/switchbutton/ISDSwitchButton.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.fanwe.library.switchbutton;
+package com.fanwe.lib.switchbutton;
 
 import android.view.View;
 
