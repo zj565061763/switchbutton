539c41b9aa18926be68e6b3d2923878bd2d0a27f
zjun <565061763@qq.com>
2018-05-08 11:12:48 +0800

    demo

--- app/src/main/java/com/fanwe/www/switchbutton/MainActivity.java
+++ app/src/main/java/com/fanwe/www/switchbutton/MainActivity.java
@@ -2,9 +2,7 @@ package com.fanwe.www.switchbutton;
 
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.fanwe.lib.adapter.FSimpleRecyclerAdapter;
 import com.fanwe.lib.adapter.viewholder.FRecyclerViewHolder;
@@ -51,16 +49,6 @@ public class MainActivity extends SDBaseActivity
                 ViewCompat.setScaleY(view.getViewChecked(), percent);
             }
         });
-
-        sb_rect.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
     }
 
 
